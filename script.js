76b9e03f420f2df03e47f6f3921e3f368c0ea399
Roshnisura <127479394+Roshnisura@users.noreply.github.com>
2023-04-12 08:34:56 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-// Todo: Make M+ M- and MC functional
-let string = "";
-let buttons = document.querySelectorAll('.button');
-Array.from(buttons).forEach((button)=>{
-  button.addEventListener('click', (e)=>{
-    if(e.target.innerHTML == '='){
-      string = eval(string);
-      document.querySelector('input').value = string;
-    }
-    else if(e.target.innerHTML == 'C'){
-      string = ""
-      document.querySelector('input').value = string;
-    }
-    else{ 
-    console.log(e.target)
-    string = string + e.target.innerHTML;
-    document.querySelector('input').value = string;
-      }
-  })
-})
+let btn = document.getElementById("shorten");
+
+btn.addEventListener('click', short);
+
+
+async function short(){
+    let longURL = document.getElementById("longurl").value;
+    let shortURL = document.getElementById("shorturl");
+
+    const result = await fetch(`https://api.shrtco.de/v2/shorten?url=${longURL}`);
+    const data = await result.json();
+
+    shortURL.value = data.result.short_link2;
+}
+
+
+let newURL = document.getElementById("shorturl");
+let coptButton = document.getElementById("copy");
+
+coptButton.onclick = ()=>{
+    newURL.select();
+
+    window.navigator.clipboard.writeText(newURL.value);
+}
